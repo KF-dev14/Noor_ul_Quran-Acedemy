917dfda36bb09e19becc252d09aacd74cf741754
KF-dev14 <khazran422@gmail.com>
2026-09-23 11:59:43 +0500

    fix: replace react-icons with native inline svg icons

--- noor-ul-quran/src/components/Navbar.jsx
+++ noor-ul-quran/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { FaFacebookF, FaWhatsapp, FaEnvelope } from 'react-icons/fa';
 
 function Navbar() {
   const location = useLocation();
@@ -14,7 +13,6 @@ function Navbar() {
     display: 'inline-flex',
     alignItems: 'center',
     justifyContent: 'center',
-    fontSize: '0.85rem',
     textDecoration: 'none',
     transition: 'all 0.2s ease-in-out'
   };
@@ -31,9 +29,11 @@ function Navbar() {
 
   return (
     <header style={{ width: '100%', fontFamily: 'sans-serif' }}>
-      {/* Top Orange Bar (Only WhatsApp, Email, & Facebook) */}
+      {/* Top Orange Bar */}
       <div style={{ backgroundColor: '#f59e0b', padding: '6px 5%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
         <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+          
+          {/* WhatsApp Icon */}
           <a 
             href="https://wa.me/923126884512" 
             target="_blank" 
@@ -41,15 +41,23 @@ function Navbar() {
             style={socialIconStyle} 
             title="WhatsApp"
           >
-            <FaWhatsapp />
+            <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981z"/>
+            </svg>
           </a>
+
+          {/* Email Icon */}
           <a 
             href="mailto:kf.dev786@gmail.com" 
             style={socialIconStyle} 
             title="Email"
           >
-            <FaEnvelope />
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"/>
+            </svg>
           </a>
+
+          {/* Facebook Icon */}
           <a 
             href="https://facebook.com" 
             target="_blank" 
@@ -57,8 +65,11 @@ function Navbar() {
             style={socialIconStyle} 
             title="Facebook"
           >
-            <FaFacebookF />
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
+            </svg>
           </a>
+
         </div>
       </div>
 
